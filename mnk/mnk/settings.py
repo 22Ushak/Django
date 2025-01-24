43e13842dfdadc49dfab51ec8ff47a0c42fa766d
22Ushak <2ushakk@gmail.com>
2025-01-24 12:06:51 +0530

    Updated django work

--- mnk/mnk/settings.py
+++ mnk/mnk/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'travello.apps.TravelloConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,7 +78,12 @@ WSGI_APPLICATION = 'mnk.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME':BASE_DIR/'db.sqlite3',
+        
+        # 'USER' : 'postgres',
+        # 'Password' : 1234,
+        # 'HOST': 'localhost',
+        # 'PORT' : 5432
     }
 }
 
@@ -117,8 +123,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
